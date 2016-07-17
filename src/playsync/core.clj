15d8a970d25b62bbd1525a5952248e161d7b0e00
vandosant <sjskender@gmail.com>
2016-07-17 15:30:15 -0600

    Add example using blocking thread

--- src/playsync/core.clj
+++ src/playsync/core.clj
@@ -7,8 +7,30 @@
 
 (def echo-chan (chan))
 
+(defn get-google []
+  (Thread/sleep 1000)
+  (slurp "http://google.com"))
+
+(defn sleep-then-print [sleep-time content]
+  (Thread/sleep sleep-time)
+  (println content))
+
+(defn sleep-then-return [sleep-time content]
+  (Thread/sleep sleep-time)
+  content)
+
 (defn -main
   [& args]
+  (println "process started")
+
   (go (println (<! echo-chan)))
-  (>!! echo-chan "ping")
-  (go (>! echo-chan "ping")))
+  (>!! echo-chan "first msg, non-blocking")
+
+  (let [t (thread (sleep-then-return 2000 "second msg, blocking"))]
+    (println (<!! t)))
+
+  (go (println (<! echo-chan)))
+  (go (println (<! echo-chan)))
+
+  (>!! echo-chan (sleep-then-return 1000 "third msg, blocking"))
+  (>!! echo-chan "fourth msg, blocking"))
